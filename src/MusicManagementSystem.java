563d8ca7f0b11031335d6f2ca220627bf72890eb
Shinyeongmin <dudals3861@gnu.ac.kr>
2021-03-29 22:37:08 +0900

    add method

--- src/MusicManagementSystem.java
+++ src/MusicManagementSystem.java
@@ -16,24 +16,75 @@ public class MusicManagementSystem
          System.out.println("3. 노래 수정");
          System.out.println("4. 재생목록 보기");
          System.out.println("5. Exit");
+         System.out.println();
          num = input.nextInt();
          switch (num)
          {
          case 1 :
-            System.out.print("노래 제목 : ");
-            String SongTitle = input.next();
-            System.out.print("가수 : ");
-            String Singer = input.next();
-            System.out.println();
-            break;
+             addMusic();
+             break;
          case 2 :
-            
+             deleteMusic();
+             break;
          case 3 :
-            
+             editMusic();
+             break;
          case 4 :
-            
+             viewMusic();
+             break;
+         case 5 :
+             viewChart();
+             break;
          }
          
       }
+            
+   }
+   public static void addMusic()
+   {
+ 	  Scanner input = new Scanner(System.in);
+ 	  
+ 	  System.out.print("노래 제목 : ");
+ 	  String songtitle = input.next();
+	  System.out.print("가수 : ");
+	  String singer = input.next();
+	  System.out.print("앨범 : ");
+ 	  String album = input.next();
+	  System.out.println("노래 제목 : "+songtitle);
+	  System.out.println("가수 : "+singer);
+	  System.out.println("앨범 : "+album);
+	  System.out.println();
+   }
+   public static void deleteMusic()
+   {
+	   Scanner input = new Scanner(System.in);
+	   
+	   System.out.print("노래 제목 : ");
+	   String songtitle = input.next();
+	   System.out.print("가수 : ");
+	   String singer = input.next();
+	   System.out.println("노래 제목 : "+songtitle);
+	   System.out.println("가수 : "+singer);
+	   System.out.println();
+   }
+   public static void editMusic()
+   {
+	   Scanner input = new Scanner(System.in);
+	   
+	   System.out.print("노래 제목 : ");
+	   String songtitle = input.next();
+	   System.out.print("가수 : ");
+	   String singer = input.next();
+	   System.out.println("노래 제목 : "+songtitle);
+	   System.out.println("가수 : "+singer);
+	   System.out.println();
+   }
+   public static void viewMusic()
+   {
+	   System.out.println();
+   }
+   public static void viewChart()
+   {
+	   System.out.println();
    }
 }
